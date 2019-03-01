4347652910185ec353ef3d3326a9f812c1b0c886
Lae <llaechen@gmail.com>
2019-03-01 20:18:43 +1300

    Add epoch.

--- src/Data/Time.hs
+++ src/Data/Time.hs
@@ -9,6 +9,10 @@ data Instant = Instant
   , getNano :: Int
   } deriving (Eq, Ord, Show)
 
+-- | 1970-01-01T00:00:00Z
+epoch :: Instant
+epoch = Instant 0 0
+
 -- | Converts an instant to number of milliseconds since 'epoch'.
 toEpochMillis :: Instant -> Integer
 toEpochMillis (Instant sec nano) =
