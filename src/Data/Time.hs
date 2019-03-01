2904449ef8f4634d014747c77a3ec7ab33c93e18
Lae <llaechen@gmail.com>
2019-03-01 14:44:53 +1300

    Add toEpochMillis.

--- src/Data/Time.hs
+++ src/Data/Time.hs
@@ -1,5 +1,6 @@
 module Data.Time
   ( Instant(..)
+  , toEpochMillis
   )
 where
 
@@ -7,3 +8,8 @@ data Instant = Instant
   { getEpochSecond :: Integer
   , getNano :: Int
   } deriving (Eq, Ord, Show)
+
+toEpochMillis :: Instant -> Integer
+toEpochMillis (Instant sec nano)
+  | sec >= 0  = sec * 1000 + fromIntegral (nano `div` 1000000)
+  | otherwise = sec * 1000 + fromIntegral (nano `div` 1000000) -- TODO
--- test/Data/TimeSpec.hs
+++ test/Data/TimeSpec.hs
@@ -18,3 +18,14 @@ instantSpec = describe "Instant" $ do
   it "greater than another when value is greater" $ do
     (Instant 1 3 `compare` Instant 1 2) `shouldBe` GT
     (Instant 2 2 `compare` Instant 1 2) `shouldBe` GT
+
+  it "`toEpochMillis` returns 0 for epoch time"
+     (toEpochMillis (Instant 0 0) `shouldBe` 0)
+
+  it "`toEpochMillis` returns correct value for time in future" $ do
+    toEpochMillis (Instant 1 2) `shouldBe` 1000
+    toEpochMillis (Instant 11 200000000) `shouldBe` 11200
+
+  it "`toEpochMillis` returns correct value for time in past" $ do
+    toEpochMillis (Instant (-1) 2) `shouldBe` 999
+    toEpochMillis (Instant 1 200000000) `shouldBe` 800
