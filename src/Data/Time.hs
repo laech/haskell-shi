eeaae74f0b5ac280a022901bf67d9e00b197b1ed
Lae <llaechen@gmail.com>
2019-03-03 08:36:37 +1300

    Add getDaysInMonth.

--- src/Data/Time.hs
+++ src/Data/Time.hs
@@ -6,6 +6,7 @@ module Data.Time
   , isLeapYear
   , Month(..)
   , monthOf
+  , getDaysInMonth
   )
 where
 
@@ -90,3 +91,22 @@ monthOf i = case i of
   11 -> pure November
   12 -> pure December
   _  -> fail ("Uknown month: " ++ show i)
+
+-- | Gets the number of days in a month.
+getDaysInMonth
+  :: Month
+  -> Bool -- ^ Is leap year?
+  -> Int
+getDaysInMonth month leapYear = case month of
+  January   -> 31
+  February  -> if leapYear then 29 else 28
+  March     -> 31
+  April     -> 30
+  May       -> 31
+  June      -> 30
+  July      -> 31
+  August    -> 31
+  September -> 30
+  October   -> 31
+  November  -> 30
+  December  -> 31
--- test/Data/TimeSpec.hs
+++ test/Data/TimeSpec.hs
@@ -85,3 +85,22 @@ monthSpec = describe "Month" $ do
   it "sorts naturally" $ do
     sort months `shouldBe` months
     [minBound .. maxBound] `shouldBe` months
+
+  describe "getDaysInMonth" $ do
+
+    let expectDaysInMonth month daysInNonLeapYear daysInLeapYear =
+          map (getDaysInMonth month) [False, True]
+            `shouldBe` [daysInNonLeapYear, daysInLeapYear]
+
+    it "January has 31 days"     (expectDaysInMonth January 31 31)
+    it "February has 28/29 days" (expectDaysInMonth February 28 29)
+    it "March has 31 days"       (expectDaysInMonth March 31 31)
+    it "April has 30 days"       (expectDaysInMonth April 30 30)
+    it "May has 31 days"         (expectDaysInMonth May 31 31)
+    it "June has 30 days"        (expectDaysInMonth June 30 30)
+    it "July has 31 days"        (expectDaysInMonth July 31 31)
+    it "August has 31 days"      (expectDaysInMonth August 31 31)
+    it "September has 30 days"   (expectDaysInMonth September 30 30)
+    it "October has 31 days"     (expectDaysInMonth October 31 31)
+    it "November has 30 days"    (expectDaysInMonth November 30 30)
+    it "December has 31 days"    (expectDaysInMonth December 31 31)
