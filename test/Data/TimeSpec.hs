72866df1dd19f4dd0f4b8ed72076f6df04fa4887
Lae <llaechen@gmail.com>
2019-03-30 21:50:56 +1300

    Refactor tests.

--- test/Data/TimeSpec.hs
+++ test/Data/TimeSpec.hs
@@ -140,49 +140,51 @@ localDateSpec = describe "LocalDate" $ do
 
   describe "toEpochDay" $ do
 
-    it "returns 0 for epoch" 
-      (toEpochDay (LocalDate 1970 1 1) `shouldBe` 0)
+    it "returns 0 for epoch" (toEpochDay (LocalDate 1970 1 1) `shouldBe` 0)
 
-    it "returns correct values for positive days" $ do
-      toEpochDay (LocalDate 1970 1 2) `shouldBe` 1
-      toEpochDay (LocalDate 1970 1 3) `shouldBe` 2
-      toEpochDay (LocalDate 1970 12 31) `shouldBe` 364
-      toEpochDay (LocalDate 1971 1 1) `shouldBe` 365
-      toEpochDay (LocalDate 2004 2 29) `shouldBe` 12477
-      toEpochDay (LocalDate 2019 3 17) `shouldBe` 17972
-      toEpochDay (LocalDate 999999999 12 31) `shouldBe` 365241780471
-      toEpochDay (LocalDate 74556927 9 2) `shouldBe` 27230639126
-      toEpochDay (LocalDate 941048034 12 12) `shouldBe` 343710017376
-      toEpochDay (LocalDate 56483748 1 8) `shouldBe` 20629545808
-      toEpochDay (LocalDate 243946412 7 19) `shouldBe` 89098878057
-      toEpochDay (LocalDate 78675278 5 11) `shouldBe` 28734835828
-      toEpochDay (LocalDate 32078367 12 25) `shouldBe` 11715663789
-      toEpochDay (LocalDate 463697857 4 1) `shouldBe` 169361445098
-      toEpochDay (LocalDate 443805972 8 24) `shouldBe` 162096083436
-      toEpochDay (LocalDate 470418915 7 16) `shouldBe` 171816261230
-      toEpochDay (LocalDate 897678790 5 23) `shouldBe` 327869726071
-      toEpochDay (LocalDate 162193233 1 15) `shouldBe` 59239142391
-      toEpochDay (LocalDate 534365546 5 28) `shouldBe` 195172288554
-
-    it "returns correct values for negative days" $ do
-      toEpochDay (LocalDate 1969 12 31) `shouldBe` (-1)
-      toEpochDay (LocalDate 1969 12 30) `shouldBe` (-2)
-      toEpochDay (LocalDate 1969 1 1) `shouldBe` (-365)
-      toEpochDay (LocalDate 1960 2 29) `shouldBe` (-3594)
-      toEpochDay (LocalDate 0 1 1) `shouldBe` (-719528)
-      toEpochDay (LocalDate 0 12 31) `shouldBe` (-719163)
-      toEpochDay (LocalDate (-400) 12 31) `shouldBe` (-865260)
-      toEpochDay (LocalDate (-1) 12 31) `shouldBe` (-719529)
-      toEpochDay (LocalDate (-1) 1 1) `shouldBe` (-719893)
-      toEpochDay (LocalDate (-999999999) 1 1) `shouldBe` (-365243219162)
-      toEpochDay (LocalDate (-44758093) 11 19) `shouldBe` (-16348276989)
-      toEpochDay (LocalDate (-302472524) 10 20) `shouldBe` (-110476540082)
-      toEpochDay (LocalDate (-984111839) 12 8) `shouldBe` (-359440187542)
-      toEpochDay (LocalDate (-750556618) 8 8) `shouldBe` (-274135894858)
-      toEpochDay (LocalDate (-882146331) 9 3) `shouldBe` (-322198050582)
-      toEpochDay (LocalDate (-785520990) 2 4) `shouldBe` (-286906369684)
-      toEpochDay (LocalDate (-26911170) 6 23) `shouldBe` (-9829822363)
-      toEpochDay (LocalDate (-296189792) 6 13) `shouldBe` (-108181819469)
+    mapM_
+      (\(localDate, epochDay) -> it
+        ("returns correct values for " ++ show localDate)
+        (toEpochDay localDate `shouldBe` epochDay)
+      )
+      [ (LocalDate 1970 1 2, 1)
+      , (LocalDate 1970 1 3, 2)
+      , (LocalDate 1970 12 31, 364)
+      , (LocalDate 1971 1 1, 365)
+      , (LocalDate 2004 2 29, 12477)
+      , (LocalDate 2019 3 17, 17972)
+      , (LocalDate 999999999 12 31, 365241780471)
+      , (LocalDate 74556927 9 2, 27230639126)
+      , (LocalDate 941048034 12 12, 343710017376)
+      , (LocalDate 56483748 1 8, 20629545808)
+      , (LocalDate 243946412 7 19, 89098878057)
+      , (LocalDate 78675278 5 11, 28734835828)
+      , (LocalDate 32078367 12 25, 11715663789)
+      , (LocalDate 463697857 4 1, 169361445098)
+      , (LocalDate 443805972 8 24, 162096083436)
+      , (LocalDate 470418915 7 16, 171816261230)
+      , (LocalDate 897678790 5 23, 327869726071)
+      , (LocalDate 162193233 1 15, 59239142391)
+      , (LocalDate 534365546 5 28, 195172288554)
+      , (LocalDate 1969 12 31, -1)
+      , (LocalDate 1969 12 30, -2)
+      , (LocalDate 1969 1 1, -365)
+      , (LocalDate 1960 2 29, -3594)
+      , (LocalDate 0 1 1, -719528)
+      , (LocalDate 0 12 31, -719163)
+      , (LocalDate (-400) 12 31, -865260)
+      , (LocalDate (-1) 12 31, -719529)
+      , (LocalDate (-1) 1 1, -719893)
+      , (LocalDate (-999999999) 1 1, -365243219162)
+      , (LocalDate (-44758093) 11 19, -16348276989)
+      , (LocalDate (-302472524) 10 20, -110476540082)
+      , (LocalDate (-984111839) 12 8, -359440187542)
+      , (LocalDate (-750556618) 8 8, -274135894858)
+      , (LocalDate (-882146331) 9 3, -322198050582)
+      , (LocalDate (-785520990) 2 4, -286906369684)
+      , (LocalDate (-26911170) 6 23, -9829822363)
+      , (LocalDate (-296189792) 6 13, -108181819469)
+      ]
 
   describe "localDateOf" $ do
 
