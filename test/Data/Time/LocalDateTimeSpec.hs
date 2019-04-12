984b8a3e934be892452820a7c4be1e6bc6b8711c
Lae <llaechen@gmail.com>
2019-04-13 08:41:00 +1200

    Rename.

--- test/Data/Time/LocalDateTimeSpec.hs
+++ test/Data/Time/LocalDateTimeSpec.hs
@@ -24,9 +24,9 @@ spec =
     describe "getEpochMilli" getEpochMilliSpec
     describe "addDays" addDaysSpec
 
-localDateTimeValid ::
+localDateTime ::
      Integer -> Int -> Int -> Int -> Int -> Int -> Int -> LocalDateTime
-localDateTimeValid year month day hour minute second nano =
+localDateTime year month day hour minute second nano =
   LocalDateTime
     (fromJust (localDateOf year month day))
     (fromJust (localTimeOf hour minute second nano))
@@ -35,21 +35,21 @@ compareSpec :: Spec
 compareSpec =
   mapM_
     test
-    [ (EQ, localDateTimeValid 1 1 1 1 1 1 1, localDateTimeValid 1 1 1 1 1 1 1)
-    , (LT, localDateTimeValid 1 1 1 1 1 1 1, localDateTimeValid 1 1 1 1 1 1 2)
-    , (LT, localDateTimeValid 1 1 1 1 1 1 1, localDateTimeValid 1 1 1 1 1 2 1)
-    , (LT, localDateTimeValid 1 1 1 1 1 1 1, localDateTimeValid 1 1 1 1 2 1 1)
-    , (LT, localDateTimeValid 1 1 1 1 1 1 1, localDateTimeValid 1 1 1 2 1 1 1)
-    , (LT, localDateTimeValid 1 1 1 1 1 1 1, localDateTimeValid 1 1 2 1 1 1 1)
-    , (LT, localDateTimeValid 1 1 1 1 1 1 1, localDateTimeValid 1 2 1 1 1 1 1)
-    , (LT, localDateTimeValid 1 1 1 1 1 1 1, localDateTimeValid 2 1 1 1 1 1 1)
-    , (GT, localDateTimeValid 1 1 1 1 1 1 2, localDateTimeValid 1 1 1 1 1 1 1)
-    , (GT, localDateTimeValid 1 1 1 1 1 2 1, localDateTimeValid 1 1 1 1 1 1 1)
-    , (GT, localDateTimeValid 1 1 1 1 2 1 1, localDateTimeValid 1 1 1 1 1 1 1)
-    , (GT, localDateTimeValid 1 1 1 2 1 1 1, localDateTimeValid 1 1 1 1 1 1 1)
-    , (GT, localDateTimeValid 1 1 2 1 1 1 1, localDateTimeValid 1 1 1 1 1 1 1)
-    , (GT, localDateTimeValid 1 2 1 1 1 1 1, localDateTimeValid 1 1 1 1 1 1 1)
-    , (GT, localDateTimeValid 2 1 1 1 1 1 1, localDateTimeValid 1 1 1 1 1 1 1)
+    [ (EQ, localDateTime 1 1 1 1 1 1 1, localDateTime 1 1 1 1 1 1 1)
+    , (LT, localDateTime 1 1 1 1 1 1 1, localDateTime 1 1 1 1 1 1 2)
+    , (LT, localDateTime 1 1 1 1 1 1 1, localDateTime 1 1 1 1 1 2 1)
+    , (LT, localDateTime 1 1 1 1 1 1 1, localDateTime 1 1 1 1 2 1 1)
+    , (LT, localDateTime 1 1 1 1 1 1 1, localDateTime 1 1 1 2 1 1 1)
+    , (LT, localDateTime 1 1 1 1 1 1 1, localDateTime 1 1 2 1 1 1 1)
+    , (LT, localDateTime 1 1 1 1 1 1 1, localDateTime 1 2 1 1 1 1 1)
+    , (LT, localDateTime 1 1 1 1 1 1 1, localDateTime 2 1 1 1 1 1 1)
+    , (GT, localDateTime 1 1 1 1 1 1 2, localDateTime 1 1 1 1 1 1 1)
+    , (GT, localDateTime 1 1 1 1 1 2 1, localDateTime 1 1 1 1 1 1 1)
+    , (GT, localDateTime 1 1 1 1 2 1 1, localDateTime 1 1 1 1 1 1 1)
+    , (GT, localDateTime 1 1 1 2 1 1 1, localDateTime 1 1 1 1 1 1 1)
+    , (GT, localDateTime 1 1 2 1 1 1 1, localDateTime 1 1 1 1 1 1 1)
+    , (GT, localDateTime 1 2 1 1 1 1 1, localDateTime 1 1 1 1 1 1 1)
+    , (GT, localDateTime 2 1 1 1 1 1 1, localDateTime 1 1 1 1 1 1 1)
     ]
   where
     test arg@(expect, a, b) = it (show arg) $ a `compare` b `shouldBe` expect
@@ -58,9 +58,9 @@ getYearSpec :: Spec
 getYearSpec =
   mapM_
     test
-    [ (localDateTimeValid 0 2 3 4 5 6 7, 0)
-    , (localDateTimeValid 1 2 3 4 5 6 7, 1)
-    , (localDateTimeValid (-1) 2 3 4 5 6 7, -1)
+    [ (localDateTime 0 2 3 4 5 6 7, 0)
+    , (localDateTime 1 2 3 4 5 6 7, 1)
+    , (localDateTime (-1) 2 3 4 5 6 7, -1)
     ]
   where
     test arg@(datetime, year) = it (show arg) $ getYear datetime `shouldBe` year
@@ -69,9 +69,9 @@ getMonthSpec :: Spec
 getMonthSpec =
   mapM_
     test
-    [ (localDateTimeValid 10 1 2 3 4 5 6, 1)
-    , (localDateTimeValid 10 7 2 3 4 5 6, 7)
-    , (localDateTimeValid 10 9 2 3 4 5 6, 9)
+    [ (localDateTime 10 1 2 3 4 5 6, 1)
+    , (localDateTime 10 7 2 3 4 5 6, 7)
+    , (localDateTime 10 9 2 3 4 5 6, 9)
     ]
   where
     test arg@(datetime, month) =
@@ -81,9 +81,7 @@ getDayOfMonthSpec :: Spec
 getDayOfMonthSpec =
   mapM_
     test
-    [ (localDateTimeValid 10 10 11 2 3 4 5, 11)
-    , (localDateTimeValid 10 10 9 2 3 4 5, 9)
-    ]
+    [(localDateTime 10 10 11 2 3 4 5, 11), (localDateTime 10 10 9 2 3 4 5, 9)]
   where
     test arg@(datetime, day) =
       it (show arg) $ getDayOfMonth datetime `shouldBe` day
@@ -92,14 +90,14 @@ getDayOfYearSpec :: Spec
 getDayOfYearSpec =
   mapM_
     test
-    [ (localDateTimeValid 1970 1 1 1 2 3 4, 1)
-    , (localDateTimeValid 1970 1 2 0 0 0 0, 2)
-    , (localDateTimeValid 1970 2 1 9 8 7 6, 32)
-    , (localDateTimeValid 1970 12 31 0 0 0 0, 365)
-    , (localDateTimeValid 2000 2 28 0 0 0 0, 59)
-    , (localDateTimeValid 2000 2 29 2 2 2 2, 60)
-    , (localDateTimeValid 2000 3 1 0 0 0 0, 61)
-    , (localDateTimeValid 2000 12 31 0 0 0 0, 366)
+    [ (localDateTime 1970 1 1 1 2 3 4, 1)
+    , (localDateTime 1970 1 2 0 0 0 0, 2)
+    , (localDateTime 1970 2 1 9 8 7 6, 32)
+    , (localDateTime 1970 12 31 0 0 0 0, 365)
+    , (localDateTime 2000 2 28 0 0 0 0, 59)
+    , (localDateTime 2000 2 29 2 2 2 2, 60)
+    , (localDateTime 2000 3 1 0 0 0 0, 61)
+    , (localDateTime 2000 12 31 0 0 0 0, 366)
     ]
   where
     test arg@(datetime, dayOfYear) =
@@ -109,9 +107,9 @@ getHourSpec :: Spec
 getHourSpec =
   mapM_
     test
-    [ (localDateTimeValid 10 10 10 0 2 3 4, 0)
-    , (localDateTimeValid 10 10 10 1 2 3 4, 1)
-    , (localDateTimeValid 10 10 10 23 2 3 4, 23)
+    [ (localDateTime 10 10 10 0 2 3 4, 0)
+    , (localDateTime 10 10 10 1 2 3 4, 1)
+    , (localDateTime 10 10 10 23 2 3 4, 23)
     ]
   where
     test arg@(datetime, hour) = it (show arg) $ getHour datetime `shouldBe` hour
@@ -120,9 +118,9 @@ getMinuteSpec :: Spec
 getMinuteSpec =
   mapM_
     test
-    [ (localDateTimeValid 10 10 10 10 0 2 3, 0)
-    , (localDateTimeValid 10 10 10 10 1 2 3, 1)
-    , (localDateTimeValid 10 10 10 10 59 2 3, 59)
+    [ (localDateTime 10 10 10 10 0 2 3, 0)
+    , (localDateTime 10 10 10 10 1 2 3, 1)
+    , (localDateTime 10 10 10 10 59 2 3, 59)
     ]
   where
     test arg@(datetime, minute) =
@@ -132,9 +130,9 @@ getSecondSpec :: Spec
 getSecondSpec =
   mapM_
     test
-    [ (localDateTimeValid 10 10 10 10 10 0 2, 0)
-    , (localDateTimeValid 10 10 10 10 10 1 2, 1)
-    , (localDateTimeValid 10 10 10 10 10 59 2, 59)
+    [ (localDateTime 10 10 10 10 10 0 2, 0)
+    , (localDateTime 10 10 10 10 10 1 2, 1)
+    , (localDateTime 10 10 10 10 10 59 2, 59)
     ]
   where
     test arg@(datetime, second) =
@@ -144,9 +142,9 @@ getNanoOfSecondSpec :: Spec
 getNanoOfSecondSpec =
   mapM_
     test
-    [ (localDateTimeValid 10 10 10 10 10 10 0, 0)
-    , (localDateTimeValid 10 10 10 10 10 10 1, 1)
-    , (localDateTimeValid 10 10 10 10 10 10 999999999, 999999999)
+    [ (localDateTime 10 10 10 10 10 10 0, 0)
+    , (localDateTime 10 10 10 10 10 10 1, 1)
+    , (localDateTime 10 10 10 10 10 10 999999999, 999999999)
     ]
   where
     test arg@(datetime, nano) =
@@ -156,16 +154,16 @@ getSecondOfDaySpec :: Spec
 getSecondOfDaySpec =
   mapM_
     test
-    [ (localDateTimeValid 9 9 9 0 0 0 9, 0)
-    , (localDateTimeValid 9 9 9 0 0 1 9, 1)
-    , (localDateTimeValid 9 9 9 0 0 59 9, 59)
-    , (localDateTimeValid 9 9 9 0 1 0 9, 60)
-    , (localDateTimeValid 9 9 9 0 1 2 9, 62)
-    , (localDateTimeValid 9 9 9 0 2 0 9, 120)
-    , (localDateTimeValid 9 9 9 0 2 1 9, 121)
-    , (localDateTimeValid 9 9 9 1 0 0 9, 3600)
-    , (localDateTimeValid 9 9 9 1 1 0 9, 3660)
-    , (localDateTimeValid 9 9 9 1 1 1 9, 3661)
+    [ (localDateTime 9 9 9 0 0 0 9, 0)
+    , (localDateTime 9 9 9 0 0 1 9, 1)
+    , (localDateTime 9 9 9 0 0 59 9, 59)
+    , (localDateTime 9 9 9 0 1 0 9, 60)
+    , (localDateTime 9 9 9 0 1 2 9, 62)
+    , (localDateTime 9 9 9 0 2 0 9, 120)
+    , (localDateTime 9 9 9 0 2 1 9, 121)
+    , (localDateTime 9 9 9 1 0 0 9, 3600)
+    , (localDateTime 9 9 9 1 1 0 9, 3660)
+    , (localDateTime 9 9 9 1 1 1 9, 3661)
     ]
   where
     test arg@(datetime, second) =
@@ -175,12 +173,12 @@ getEpochDaySpec :: Spec
 getEpochDaySpec =
   mapM_
     test
-    [ (localDateTimeValid 1970 1 1 0 0 0 0, 0)
-    , (localDateTimeValid 1970 1 1 9 9 9 9, 0)
-    , (localDateTimeValid 1970 1 2 0 0 0 0, 1)
-    , (localDateTimeValid 1970 1 2 9 9 9 9, 1)
-    , (localDateTimeValid 1969 12 31 0 0 0 0, -1)
-    , (localDateTimeValid 1969 12 31 9 9 9 9, -1)
+    [ (localDateTime 1970 1 1 0 0 0 0, 0)
+    , (localDateTime 1970 1 1 9 9 9 9, 0)
+    , (localDateTime 1970 1 2 0 0 0 0, 1)
+    , (localDateTime 1970 1 2 9 9 9 9, 1)
+    , (localDateTime 1969 12 31 0 0 0 0, -1)
+    , (localDateTime 1969 12 31 9 9 9 9, -1)
     ]
   where
     test arg@(datetime, day) =
@@ -190,12 +188,12 @@ getEpochSecondSpec :: Spec
 getEpochSecondSpec =
   mapM_
     test
-    [ (localDateTimeValid 1970 1 1 0 0 0 0, 0)
-    , (localDateTimeValid 1970 1 1 0 0 0 9, 0)
-    , (localDateTimeValid 1970 1 1 0 0 1 0, 1)
-    , (localDateTimeValid 1970 1 1 0 0 1 9, 1)
-    , (localDateTimeValid 1969 12 31 23 59 59 0, -1)
-    , (localDateTimeValid 1969 12 31 23 59 59 9, -1)
+    [ (localDateTime 1970 1 1 0 0 0 0, 0)
+    , (localDateTime 1970 1 1 0 0 0 9, 0)
+    , (localDateTime 1970 1 1 0 0 1 0, 1)
+    , (localDateTime 1970 1 1 0 0 1 9, 1)
+    , (localDateTime 1969 12 31 23 59 59 0, -1)
+    , (localDateTime 1969 12 31 23 59 59 9, -1)
     ]
   where
     test arg@(datetime, second) =
@@ -205,12 +203,12 @@ getEpochMilliSpec :: Spec
 getEpochMilliSpec =
   mapM_
     test
-    [ (localDateTimeValid 1970 1 1 0 0 0 0, 0)
-    , (localDateTimeValid 1970 1 1 0 0 0 999999, 0)
-    , (localDateTimeValid 1970 1 1 0 0 0 1000000, 1)
-    , (localDateTimeValid 1970 1 1 0 0 0 1999999, 1)
-    , (localDateTimeValid 1969 12 31 23 59 59 999999999, -1)
-    , (localDateTimeValid 1969 12 31 23 59 59 999000000, -1)
+    [ (localDateTime 1970 1 1 0 0 0 0, 0)
+    , (localDateTime 1970 1 1 0 0 0 999999, 0)
+    , (localDateTime 1970 1 1 0 0 0 1000000, 1)
+    , (localDateTime 1970 1 1 0 0 0 1999999, 1)
+    , (localDateTime 1969 12 31 23 59 59 999999999, -1)
+    , (localDateTime 1969 12 31 23 59 59 999000000, -1)
     ]
   where
     test arg@(datetime, milli) =
@@ -220,15 +218,15 @@ addDaysSpec :: Spec
 addDaysSpec =
   mapM_
     test
-    [ (localDateTimeValid 1970 1 1 0 0 0 0, 0, localDateTimeValid 1970 1 1 0 0 0 0)
-    , (localDateTimeValid 1970 1 1 9 8 7 6, 1, localDateTimeValid 1970 1 2 9 8 7 6)
-    , (localDateTimeValid 1970 1 1 0 0 0 0, 2, localDateTimeValid 1970 1 3 0 0 0 0)
-    , (localDateTimeValid 1970 1 1 0 0 0 0, -1, localDateTimeValid 1969 12 31 0 0 0 0)
-    , (localDateTimeValid 1970 1 1 0 0 0 0, -2, localDateTimeValid 1969 12 30 0 0 0 0)
-    , (localDateTimeValid 2000 2 28 5 6 7 8, 1, localDateTimeValid 2000 2 29 5 6 7 8)
-    , (localDateTimeValid 2000 2 28 0 0 0 0, 2, localDateTimeValid 2000 3 1 0 0 0 0)
-    , (localDateTimeValid 2000 1 1 0 0 0 1, 365, localDateTimeValid 2000 12 31 0 0 0 1)
-    , (localDateTimeValid 2000 1 1 0 0 0 2, 366, localDateTimeValid 2001 1 1 0 0 0 2)
+    [ (localDateTime 1970 1 1 0 0 0 0, 0, localDateTime 1970 1 1 0 0 0 0)
+    , (localDateTime 1970 1 1 9 8 7 6, 1, localDateTime 1970 1 2 9 8 7 6)
+    , (localDateTime 1970 1 1 0 0 0 0, 2, localDateTime 1970 1 3 0 0 0 0)
+    , (localDateTime 1970 1 1 0 0 0 0, -1, localDateTime 1969 12 31 0 0 0 0)
+    , (localDateTime 1970 1 1 0 0 0 0, -2, localDateTime 1969 12 30 0 0 0 0)
+    , (localDateTime 2000 2 28 5 6 7 8, 1, localDateTime 2000 2 29 5 6 7 8)
+    , (localDateTime 2000 2 28 0 0 0 0, 2, localDateTime 2000 3 1 0 0 0 0)
+    , (localDateTime 2000 1 1 0 0 0 1, 365, localDateTime 2000 12 31 0 0 0 1)
+    , (localDateTime 2000 1 1 0 0 0 2, 366, localDateTime 2001 1 1 0 0 0 2)
     ]
   where
     test arg@(fromDateTime, days, toDateTime) =
