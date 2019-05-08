d57a6751736c3c4d9f90d7e5087026ce30e64d6b
Lae <llaechen@gmail.com>
2019-05-08 22:45:39 +1200

    OffsetDateTime and some tests.

--- src/Data/Time/OffsetDateTime.hs
+++ src/Data/Time/OffsetDateTime.hs
@@ -4,13 +4,17 @@
 module Data.Time.OffsetDateTime
   ( OffsetDateTime
   , module Data.Time.Base
+  , module Data.Time.LocalDate
+  , module Data.Time.LocalTime
   , module Data.Time.LocalDateTime
   , module Data.Time.Offset
   ) where
 
 import Control.Monad.Fail
 import Data.Time.Base
+import Data.Time.LocalDate
 import Data.Time.LocalDateTime
+import Data.Time.LocalTime
 import Data.Time.Offset
 
 -- | A date and time with an offset.
@@ -26,6 +30,77 @@ instance MonadFail m => FromDateTime (Offset -> m OffsetDateTime) where
 
 instance FromLocalDateTime (Offset -> OffsetDateTime) where
   fromLocalDateTime = flip OffsetDateTime
+
+modifyLocalDateTime' ::
+     HasLocalDateTime a => (x -> LocalDateTime -> LocalDateTime) -> x -> a -> a
+modifyLocalDateTime' f x = modifyLocalDateTime (f x . getLocalDateTime)
+
+instance HasLocalDateTime OffsetDateTime where
+  getLocalDateTime (OffsetDateTime _ dt) = dt
+  setLocalDateTime dt o@(OffsetDateTime offset dt')
+    | dt == dt' = o
+    | otherwise = OffsetDateTime offset dt
+
+instance HasLocalDate OffsetDateTime where
+  getLocalDate = getLocalDate . getLocalDateTime
+  setLocalDate = modifyLocalDateTime' setLocalDate
+
+instance HasLocalTime OffsetDateTime where
+  getLocalTime = getLocalTime . getLocalDateTime
+  setLocalTime = modifyLocalDateTime' setLocalTime
+
+instance HasYear OffsetDateTime where
+  getYear = getYear . getLocalDateTime
+  setYear = modifyLocalDateTime' setYear
+
+instance HasMonth OffsetDateTime where
+  getMonth = getMonth . getLocalDateTime
+  setMonth = modifyLocalDateTime' setMonth
+  addMonths = modifyLocalDateTime' addMonths
+
+instance HasDayOfMonth OffsetDateTime where
+  getDayOfMonth = getDayOfMonth . getLocalDateTime
+
+instance HasDayOfYear OffsetDateTime where
+  getDayOfYear = getDayOfYear . getLocalDateTime
+
+instance AddDays OffsetDateTime where
+  addDays = modifyLocalDateTime' addDays
+
+instance HasHour OffsetDateTime where
+  getHour = getHour . getLocalDateTime
+  addHours = modifyLocalDateTime' addHours
+
+instance HasMinute OffsetDateTime where
+  getMinute = getMinute . getLocalDateTime
+  addMinutes = modifyLocalDateTime' addMinutes
+
+instance HasSecond OffsetDateTime where
+  getSecond = getSecond . getLocalDateTime
+  addSeconds = modifyLocalDateTime' addSeconds
+
+instance HasSecondOfDay OffsetDateTime where
+  getSecondOfDay = getSecondOfDay . getLocalDateTime
+
+instance HasNanoOfSecond OffsetDateTime where
+  getNanoOfSecond = getNanoOfSecond . getLocalDateTime
+  addNanos = modifyLocalDateTime' addNanos
+
+instance HasNanoOfDay OffsetDateTime where
+  getNanoOfDay = getNanoOfDay . getLocalDateTime
+
+getEpochLocalDateTime :: OffsetDateTime -> LocalDateTime
+getEpochLocalDateTime (OffsetDateTime offset dt) =
+  addSeconds (-(getTotalOffsetSeconds offset)) dt
+
+instance HasEpochDay OffsetDateTime where
+  getEpochDay = getEpochDay . getEpochLocalDateTime
+
+instance HasEpochSecond OffsetDateTime where
+  getEpochSecond = getEpochSecond . getEpochLocalDateTime
+
+instance HasEpochMilli OffsetDateTime where
+  getEpochMilli = getEpochMilli . getEpochLocalDateTime
 
 instance Show OffsetDateTime where
   show (OffsetDateTime offset dt) = show dt ++ show offset
--- test/Data/Time/OffsetDateTimeSpec.hs
+++ test/Data/Time/OffsetDateTimeSpec.hs
@@ -1,12 +1,44 @@
 module Data.Time.OffsetDateTimeSpec where
 
 import Data.Maybe
+import Data.Time.Month
 import Data.Time.OffsetDateTime
 import Test.Hspec
 
 spec :: Spec
-spec = describe "OffsetDateTime" $ do
-  describe "show" showSpec
+spec =
+  describe "OffsetDateTime" $ do
+    describe "show" showSpec
+    describe "getYear" getYearSpec
+    describe "getMonth" getMonthSpec
+    describe "getDayOfMonth" getDayOfMonthSpec
+    describe "getDayOfYear" getDayOfYearSpec
+    describe "getHour" getHourSpec
+    describe "getMinute" getMinuteSpec
+    describe "getSecond" getSecondSpec
+    describe "getNanoOfSecond" getNanoOfSecondSpec
+    describe "getLocalDate" getLocalDateSpec
+    describe "getLocalTime" getLocalTimeSpec
+    describe "getLocalDateTime" getLocalDateTimeSpec
+    describe "getSecondOfDay" getSecondOfDaySpec
+    describe "getNanoOfDay" getNanoOfDaySpec
+    describe "getEpochDay" getEpochDaySpec
+    describe "getEpochSecond" getEpochSecondSpec
+
+--    describe "compare" compareSpec
+--    describe "getEpochMilli" getEpochMilliSpec
+--    describe "addNanos" addNanosSpec
+--    describe "addSeconds" addSecondsSpec
+--    describe "addMinutes" addMinutesSpec
+--    describe "addHours" addHoursSpec
+--    describe "addDays" addDaysSpec
+--    describe "addMonths" addMonthsSpec
+--    describe "addYears" addYearsSpec
+--    describe "setYear" setYearSpec
+--    describe "setMonth" setMonthSpec
+--    describe "setLocalDate" setLocalDateSpec
+--    describe "setLocalTime" setLocalTimeSpec
+--    describe "setLocalDateTime" setLocalDateTimeSpec
 
 offsetDateTime ::
      Integer
@@ -37,3 +69,127 @@ showSpec =
   where
     test :: (String, OffsetDateTime) -> Spec
     test (str, dt) = it (show str) $ show dt `shouldBe` str
+
+getYearSpec :: Spec
+getYearSpec =
+  it "should return the correct year" $
+  getYear (offsetDateTime 1970 1 1 2 3 4 5 utcOffset) `shouldBe` 1970
+
+getMonthSpec :: Spec
+getMonthSpec =
+  it "should return the correct month" $
+  getMonth (offsetDateTime 1970 12 1 2 3 4 5 utcOffset) `shouldBe` December
+
+getDayOfMonthSpec :: Spec
+getDayOfMonthSpec =
+  it "should return the correct day of month" $
+  getDayOfMonth (offsetDateTime 1970 1 9 2 3 4 5 utcOffset) `shouldBe` 9
+
+getDayOfYearSpec :: Spec
+getDayOfYearSpec =
+  it "should return the correct day of year" $
+  getDayOfYear (offsetDateTime 1970 12 31 2 3 4 5 utcOffset) `shouldBe` 365
+
+getHourSpec :: Spec
+getHourSpec =
+  it "should return the correct hour" $
+  getHour (offsetDateTime 1970 12 31 2 3 4 5 utcOffset) `shouldBe` 2
+
+getMinuteSpec :: Spec
+getMinuteSpec =
+  it "should return the correct minute" $
+  getMinute (offsetDateTime 1970 12 31 2 3 4 5 utcOffset) `shouldBe` 3
+
+getSecondSpec :: Spec
+getSecondSpec =
+  it "should return the correct second" $
+  getSecond (offsetDateTime 1970 12 31 2 3 4 5 utcOffset) `shouldBe` 4
+
+getNanoOfSecondSpec :: Spec
+getNanoOfSecondSpec =
+  it "should return the correct nano of second" $
+  getNanoOfSecond (offsetDateTime 1970 12 31 2 3 4 5 utcOffset) `shouldBe` 5
+
+getLocalDateSpec :: Spec
+getLocalDateSpec =
+  it "should return the correct local date" $
+  getLocalDate (offsetDateTime 1970 12 31 2 3 4 5 utcOffset) `shouldBe`
+  fromJust (fromDate 1970 12 31)
+
+getLocalTimeSpec :: Spec
+getLocalTimeSpec =
+  it "should return the correct local time" $
+  getLocalTime (offsetDateTime 1970 12 31 2 3 4 5 utcOffset) `shouldBe`
+  fromJust (fromTime 2 3 4 5)
+
+getLocalDateTimeSpec :: Spec
+getLocalDateTimeSpec =
+  it "should return the correct local date time" $
+  getLocalDateTime (offsetDateTime 1970 12 31 2 3 4 5 (offsetOfSeconds' 3600)) `shouldBe`
+  fromJust (fromDateTime 1970 12 31 2 3 4 5)
+
+getSecondOfDaySpec :: Spec
+getSecondOfDaySpec =
+  it "should return the correct second of day" $
+  getSecondOfDay (offsetDateTime 1970 12 31 0 1 4 5 (offsetOfSeconds' 3600)) `shouldBe`
+  64
+
+getNanoOfDaySpec :: Spec
+getNanoOfDaySpec =
+  it "should return the correct nano of day" $
+  getNanoOfDay (offsetDateTime 1970 12 31 0 1 4 5 (offsetOfSeconds' 3600)) `shouldBe`
+  64000000005
+
+getEpochDaySpec :: Spec
+getEpochDaySpec =
+  mapM_
+    test
+    [ (offsetDateTime 1970 1 1 0 0 0 0 utcOffset, 0)
+    , (offsetDateTime 1970 1 1 12 0 0 0 utcOffset, 0)
+    , (offsetDateTime 1970 1 1 12 0 0 0 (offsetOfSeconds' 43200), 0)
+    , (offsetDateTime 1970 1 1 12 0 0 0 (offsetOfSeconds' (-43200)), 1)
+    , (offsetDateTime 1970 1 2 0 0 0 0 utcOffset, 1)
+    , (offsetDateTime 1970 1 1 23 59 59 0 utcOffset, 0)
+    , (offsetDateTime 1970 1 1 23 59 59 0 (offsetOfSeconds' (-1)), 1)
+    , (offsetDateTime 1970 1 1 0 0 0 0 (offsetOfSeconds' 1), -1)
+    ]
+  where
+    test arg@(datetime, day) =
+      it (show arg) $ getEpochDay datetime `shouldBe` day
+
+getEpochSecondSpec :: Spec
+getEpochSecondSpec =
+  mapM_
+    test
+    [ (offsetDateTime 1970 1 1 0 0 0 0 utcOffset, 0)
+    , (offsetDateTime 1970 1 1 0 0 0 0 (offsetOfSeconds' 1), -1)
+    , (offsetDateTime 1970 1 1 0 0 0 0 (offsetOfSeconds' (-1)), 1)
+    , (offsetDateTime 1970 1 1 12 0 0 0 (offsetOfSeconds' 43200), 0)
+    , (offsetDateTime 1970 1 1 12 0 0 0 (offsetOfSeconds' 43199), 1)
+    , (offsetDateTime 1970 1 1 12 0 0 0 (offsetOfSeconds' 43201), -1)
+    , (offsetDateTime 1970 1 2 0 0 0 0 utcOffset, 86400)
+    , (offsetDateTime 1970 1 1 23 59 59 0 utcOffset, 86399)
+    , (offsetDateTime 1970 1 1 23 59 59 0 (offsetOfSeconds' (-1)), 86400)
+    ]
+  where
+    test arg@(datetime, second) =
+      it (show arg) $ getEpochSecond datetime `shouldBe` second
+
+getEpochMilliSpec :: Spec
+getEpochMilliSpec =
+  mapM_
+    test
+    [ (offsetDateTime 1970 1 1 0 0 0 0 utcOffset, 0)
+    , (offsetDateTime 1970 1 1 0 0 0 2000000 utcOffset, 2)
+    , (offsetDateTime 1970 1 1 0 0 0 0 (offsetOfSeconds' 1), -1)
+    , (offsetDateTime 1970 1 1 0 0 0 0 (offsetOfSeconds' (-1)), 1)
+    , (offsetDateTime 1970 1 1 12 0 0 1 (offsetOfSeconds' 43200), 0)
+    , (offsetDateTime 1970 1 1 12 0 0 1000000 (offsetOfSeconds' 43199), 1001)
+    , (offsetDateTime 1970 1 1 12 0 0 0 (offsetOfSeconds' 43201), -1000)
+    , (offsetDateTime 1970 1 2 0 0 0 0 utcOffset, 86400000)
+    , (offsetDateTime 1970 1 1 11 59 59 0 utcOffset, 86339000)
+    , (offsetDateTime 1970 1 1 11 59 59 0 (offsetOfSeconds' (-1)), 86400000)
+    ]
+  where
+    test arg@(datetime, milli) =
+      it (show arg) $ getEpochMilli datetime `shouldBe` milli
